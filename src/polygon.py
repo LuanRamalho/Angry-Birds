44d58d62f22310bfddc972265db618d0eaa5f416
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2026-03-30 13:58:25 -0300

    Update polygon.py

--- src/polygon.py
+++ src/polygon.py
@@ -1,58 +1,24 @@
-import pymunk as pm
-from pymunk import Vec2d
 import pygame
-import math
-
 
 class Polygon():
-    def __init__(self, pos, length, height, space, mass=5.0):
-        moment = 1000
-        body = pm.Body(mass, moment)
-        body.position = Vec2d(*pos)
-        shape = pm.Poly.create_box(body, (length, height))
-        shape.color = (0, 0, 255)
-        shape.friction = 0.5
-        shape.collision_type = 2
-        space.add(body, shape)
-        self.body = body
-        self.shape = shape
-        wood = pygame.image.load("../resources/images/wood.png").convert_alpha()
-        wood2 = pygame.image.load("../resources/images/wood2.png").convert_alpha()
-        rect = pygame.Rect(251, 357, 86, 22)
-        self.beam_image = wood.subsurface(rect).copy()
-        rect = pygame.Rect(16, 252, 22, 84)
-        self.column_image = wood2.subsurface(rect).copy()
+    def __init__(self, pos, length, height, space=None, mass=5.0):
+        # pos agora é uma tupla (x, y)
+        self.pos = pygame.Vector2(pos[0], pos[1])
+        self.width = length
+        self.height = height
+        self.active = True
+        
+        # Criamos um Rect para colisões simples
+        # Como o level.py usa coordenadas de física (Y sobe), 
+        # o desenho será ajustado no main
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
 
-    def to_pygame(self, p):
-        """Convert pymunk to pygame coordinates"""
-        return int(p.x), int(-p.y+600)
-
-    def draw_poly(self, element, screen):
-        """Draw beams and columns"""
-        poly = self.shape
-        ps = poly.get_vertices()
-        ps.append(ps[0])
-        ps = map(self.to_pygame, ps)
-        ps = list(ps)
-        color = (255, 0, 0)
-        pygame.draw.lines(screen, color, False, ps)
-        if element == 'beams':
-            p = poly.body.position
-            p = Vec2d(*self.to_pygame(p))
-            angle_degrees = math.degrees(poly.body.angle) + 180
-            rotated_logo_img = pygame.transform.rotate(self.beam_image,
-                                                       angle_degrees)
-            offset = Vec2d(*rotated_logo_img.get_size()) / 2.
-            p = p - offset
-            np = p
-            screen.blit(rotated_logo_img, (np.x, np.y))
-        if element == 'columns':
-            p = poly.body.position
-            p = Vec2d(*self.to_pygame(p))
-            angle_degrees = math.degrees(poly.body.angle) + 180
-            rotated_logo_img = pygame.transform.rotate(self.column_image,
-                                                       angle_degrees)
-            offset = Vec2d(*rotated_logo_img.get_size()) / 2.
-            p = p - offset
-            np = p
-            screen.blit(rotated_logo_img, (np.x, np.y))
+    def draw(self, screen, image, height_screen):
+        if self.active:
+            # Inverte o Y para desenhar corretamente no Pygame
+            draw_y = height_screen - self.pos.y - self.height
+            if image:
+                img_scaled = pygame.transform.scale(image, (self.width, self.height))
+                screen.blit(img_scaled, (self.pos.x, draw_y))
+            else:
+                pygame.draw.rect(screen, (139, 69, 19), (self.pos.x, draw_y, self.width, self.height))
